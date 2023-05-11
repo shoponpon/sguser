b53478f30c5da14103297f52ffcb1a792ca6c76a
Shoponpon <oishi.sho.1994@gmail.com>
2023-05-11 16:17:02 +0900

    fix: 変数名を変更

--- main.sh
+++ main.sh
@@ -8,16 +8,16 @@ while IFS=$'\n' read -r line; do
     values[i]="${line}"
     ((++i))
 done < $PROFILE
-account=${values[0]}
-address=${values[1]}
+name=${values[0]}
+email=${values[1]}
 
 # debug
-echo "set account => "$account
-echo "set address => "$address
+echo "set name => "$name
+echo "set email => "$email
 
 # globalアカウントを変更
-git config --global user.name "$account"
-git config --global user.email "$address"
+git config --global user.name "$name"
+git config --global user.email "$email"
 
 # 確認
 git config --global -l
